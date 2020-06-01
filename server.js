366ba33bc331c11fa19894193deb4b2ce2b26a29
imvamsi <krrishvamsi7@gmail.com>
2020-06-01 16:06:14 -0400

    DB connected

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express();
 require('dotenv').config();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex: true })
+.then(() => console.log('DB connected'))
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`)
+  });
+
 
 app.get('/', (req, res) => {
     res.send('heollo from node');
