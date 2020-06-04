5b57a8ef29ae6b80199a34590172c32f84efe687
imvamsi <krrishvamsi7@gmail.com>
2020-06-04 15:28:38 -0400

    get category api working

--- controllers/category.js
+++ controllers/category.js
@@ -10,3 +10,30 @@ exports.create = (req, res) => {
         res.json({data});
     })
 }
+
+exports.categoryById = (req, res, next, id) => {
+   Category.findById(id).exec((err, category) => {
+       if(err) {
+           return res.status(400).json({
+               msg: "catgeory could not be found"
+           });
+       }
+       req.category = category;
+       next();
+   })
+}
+// exports.categoryById = (req, res, next, id) => {
+//     Category.findById(id).exec((err, category) => {
+//         if (err || !category) {
+//             return res.status(400).json({
+//                 error: "Category does not exist"
+//             });
+//         }
+//         req.category = category;
+//         next();
+//     });
+// };
+
+exports.read = (req, res) => {
+    return res.json(req.category);
+}
--- routes/category.js
+++ routes/category.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const Category = require
 const router = express.Router();
-const {create} = require('../controllers/category');
+const {create, read, categoryById} = require('../controllers/category');
 const {requireSignIn, isAdmin, isAuth} = require('../controllers/auth')
 const { userById } = require("../controllers/user");
 
+
+router.get("/category/:categoryId", read);
+// router.get("/category/:categoryId", async (req, res) => {
+//     try {
+//       const category = await Category.find({ category: req.category.id });
+//       res.json(category);
+//     } catch (error) {
+//       res.status(500).send("server error");
+//     }
+//   });
 router.post('/category/create/:userId', requireSignIn, isAuth, isAdmin, create);
+router.param("categoryId", categoryById);
 router.param("userId", userById);
+
 
 module.exports = router;
