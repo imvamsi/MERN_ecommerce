d1e9c5f49846b38b8bf363c44022c77ad99fc039
imvamsi <krrishvamsi7@gmail.com>
2020-06-04 17:08:37 -0400

    sort products through query params working

--- controllers/product.js
+++ controllers/product.js
@@ -126,7 +126,7 @@ if no params are sent, everything is returned
 exports.list = (req, res) => {
     let order = req.query.order ? req.query.order : 'asc';
     let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
-    let limit = req.query.limit ? req.query.limit : 6;
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6;
 
     Product.find()
            .select("-photo")
