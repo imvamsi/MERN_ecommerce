7857a624f5d95e76696830a8efc274862cbe8171
imvamsi <krrishvamsi7@gmail.com>
2020-06-04 13:48:23 -0400

    get product working

--- routes/product.js
+++ routes/product.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const {create} = require('../controllers/product');
 const {requireSignIn, isAdmin, isAuth} = require('../controllers/auth')
 const { userById } = require("../controllers/user");
+const { productById, read }  = require('../controllers/product')
 
+router.get('/product/:productId', read);
 router.post('/product/create/:userId', requireSignIn, isAuth, isAdmin, create);
 router.param("userId", userById);
-
+router.param('productId', productById);
 module.exports = router;
